0e736c2c4d6cd8d5d5d0f29e8d661c6b3d21596c
Rob <nomastalgic@gmail.com>
2024-02-29 17:13:30 -0500

    uploaded at 1709244807

--- article/js/scripts.js
+++ article/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - One Page Wonder v6.0.5 (https://startbootstrap.com/theme/one-page-wonder)
+* Start Bootstrap - Blog Home v5.0.8 (https://startbootstrap.com/template/blog-home)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
